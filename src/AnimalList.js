7acedc3daecdb6b88b45518575e16829df1fcecb
Adelaide Yoder <adelaide.yoder@gmail.com>
2018-07-12 15:33:00 -0500

    finished the Challenge: Functional Presentation Components at the end of Chapter 4

---
+++ src/AnimalList.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react"
+import Animal from "./Animal"
+
+
+export default class AnimalList extends Component {
+    state = {
+        animals: [
+            { id: 1, name: "Doodles" },
+            { id: 2, name: "Jack" },
+            { id: 3, name: "Angus" },
+            { id: 4, name: "Henley" },
+            { id: 5, name: "Derkins" },
+            { id: 6, name: "Checkers" }
+        ]
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                
+                <ul>
+                    {
+                        this.state.animals.map(animal => 
+                            {return <Animal key={animal.id} animal={animal} />})
+                    }
+                </ul>
+                
+            </React.Fragment>
+                )
+            }
+        }
---
+++ src/LocationList.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react"
+import Location from "./Location"
+
+
+export default class LocationList extends Component {
+    state = {
+        locations: [
+            { id: 1, name: "Nashville North" },
+            { id: 2, name: "Nashville South" }
+        ]
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <ul>
+                {
+                    this.state.locations.map(location =>  
+                        {return <Location key={location.id} location={location} />})
+                }
+                </ul>
+            </React.Fragment>
+        )
+    }
+}
